369144a0290b7635e3465455d1e23b763de3107c
Prithvi Shivaraman <git.prithvi@slogin.shivaraman.net>
2025-02-09 16:06:28 -0500

    Changed URL

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,7 @@
 const HomePage = () => {
     return (
         <div className={"h-screen flex flex-column justify-center items-center"}>
-            <h1 className={"text-3xl font-bold underline dark:text-white"}>Hello World!</h1>
+            <h1 className={"text-3xl font-bold underline dark:text-white"}>Sauna Sim</h1>
         </div>
     )
 }
