59a1fca6f52bee778c3c439a0d42ae8826b9956e
Prithvi Shivaraman <git.prithvi@slogin.shivaraman.net>
2025-02-09 19:29:53 -0500

    Added primereact

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,16 @@ import {createRoot} from "react-dom/client";
 import {BrowserRouter, Route, Routes} from "react-router";
 import HomePage from "./pages/home.jsx";
 import './assets/style/index.css';
+import {PrimeReactProvider} from "primereact/api";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <div>
+        <PrimeReactProvider>
             <BrowserRouter>
                 <Routes>
                     <Route index element={<HomePage/>}/>
                 </Routes>
             </BrowserRouter>
-        </div>
+        </PrimeReactProvider>
     </StrictMode>
 )
